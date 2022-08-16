4ce2398efde38a37f056737eb6a614ef71faf290
joyoj19 <huangyue.nz@gmail.com>
2022-08-16 20:15:43 +1200

    client chat box

--- src/Login.java
+++ src/Login.java
@@ -1,11 +1,21 @@
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.net.UnknownHostException;
 
 public class Login {
 
     static void startChat(JFrame login, String loginName) {
-        System.out.println(loginName);
+        try {
+            ChatClient client = new ChatClient(loginName);
+            login.setVisible(false);
+            login.dispose();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
