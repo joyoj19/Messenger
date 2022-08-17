50d2d98e6812083d0d32998ca6ebb2992a4eb94d
joyoj19 <huangyue.nz@gmail.com>
2022-08-17 17:38:49 +1200

    chat server running locally

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class ChatClient extends JFrame {
@@ -13,6 +16,9 @@ public class ChatClient extends JFrame {
 
     JButton send;
     JButton logout;
+
+    DataInputStream in;
+    DataOutputStream out;
 
     public ChatClient(String loginName)throws UnknownHostException, IOException {
         super(loginName);
@@ -32,6 +38,37 @@ public class ChatClient extends JFrame {
 
         send = new JButton("Send");
         logout = new JButton("Logout");
+
+        send.addActionListener(event -> {
+            try {
+                if (sendMessage.getText().length() > 0) {
+                    out.writeUTF(loginName + " DATA" + sendMessage.getText());
+                }
+                sendMessage.setText("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        logout.addActionListener(event -> {
+            try {
+                if (sendMessage.getText().length() > 0) {
+                    out.writeUTF(loginName + " LOUOUT");
+
+                    System.exit(1);
+                }
+                sendMessage.setText("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Socket socket = new Socket("127.0.0.1", 5217);
+
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+
+        out.writeUTF(loginName);
 
         setup();
 
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,2 +1,44 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ChatServer {
+    ArrayList<String> loginNames;
+    ArrayList<Socket> clientSockets;
+
+    ChatServer() throws IOException{
+        ServerSocket server = new ServerSocket(5217);
+        loginNames = new ArrayList<>();
+        clientSockets = new ArrayList<>();
+        while(true) {
+            Socket clientSocket = server.accept();
+            Client client = new Client(clientSocket);
+        }
+    }
+
+    class Client extends Thread {
+        Socket clientSocket;
+
+        DataInputStream in;
+        DataOutputStream out;
+
+        Client(Socket client) throws IOException {
+            clientSocket = client;
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+
+            String loginName = in.readUTF();
+            System.out.println("Login Name: " + loginName);
+
+            loginNames.add(loginName);
+            clientSockets.add(clientSocket);
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        new ChatServer();
+    }
 }
